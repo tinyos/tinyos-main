1d6caaa2e06e691e6cf07373fe8ceef23f0b76e6
idgay <idgay@285f258d-8064-b571-fbc6-9656dc998e90>
2007-02-09 21:31:12 +0000

    missing file
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@3069 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ tos/sensorboards/mts300/mts300.h
@@ -0,0 +1,9 @@
+#ifndef MTS300_H
+#define MTS300_H
+
+#define UQ_ACCEL_RESOURCE "mts300.accel"
+#define UQ_PHOTO_RESOURCE "mts300.photo"
+#define UQ_TEMP_RESOURCE "mts300.temp"
+#define UQ_PHOTOTEMP_RESOURCE "mts300.phototemp"
+
+#endif
