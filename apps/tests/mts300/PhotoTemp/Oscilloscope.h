cb5e0cdc213e7dcdfeff35dfe74f4cfc4f47d945
idgay <idgay@285f258d-8064-b571-fbc6-9656dc998e90>
2007-06-07 16:59:34 +0000

    missing file
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@3317 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ apps/tests/mts300/PhotoTemp/Oscilloscope.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2006 Intel Corporation
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached INTEL-LICENSE     
+ * file. If you do not find these files, copies can be found by writing to
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300, Berkeley, CA, 
+ * 94704.  Attention:  Intel License Inquiry.
+ */
+
+// @author David Gay
+
+#ifndef OSCILLOSCOPE_H
+#define OSCILLOSCOPE_H
+
+enum {
+  /* Number of readings per message. If you increase this, you may have to
+     increase the message_t size. */
+  NREADINGS = 10,
+
+  /* Default sampling period. */
+  DEFAULT_INTERVAL = 256,
+
+  AM_OSCILLOSCOPE = 0x93
+};
+
+typedef nx_struct oscilloscope {
+  nx_uint16_t version; /* Version of the interval. */
+  nx_uint16_t interval; /* Samping period. */
+  nx_uint16_t id; /* Mote id of sending mote. */
+  nx_uint16_t count; /* The readings are samples count * NREADINGS onwards */
+  nx_uint16_t readings[NREADINGS];
+} oscilloscope_t;
+
+#endif
