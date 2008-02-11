ea86cd7701df897c38148cb13ce44cc9650ab497
regehr <regehr@285f258d-8064-b571-fbc6-9656dc998e90>
2008-02-11 20:46:38 +0000

    Annotation improvements, and add deputy_no_annots.h which causes CPP to take an app.c and squash all annotations.
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@3768 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ tos/lib/safe/include/deputy_no_annots.h
@@ -0,0 +1,6 @@
+#define __DEPUTY_BOUND(__lo,__hi)              
+#define __DEPUTY_COUNT(__n)                    
+#define __DEPUTY_SINGLE(__n)                   
+#define __DEPUTY_TRUSTED_CAST(__type,__expr)   
+
+#define TC(__type,__expr)                      __DEPUTY_TRUSTED_CAST(__type,__expr)
