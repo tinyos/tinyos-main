9feff7a55db801a9d74ade7a2f727dc26474ca34
regehr <regehr@285f258d-8064-b571-fbc6-9656dc998e90>
2008-02-12 21:16:00 +0000

    annotation work
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@3771 285f258d-8064-b571-fbc6-9656dc998e90

--- tos/lib/safe/include/deputy_no_annots.h
+++ tos/lib/safe/include/deputy_no_annots.h
@@ -1,9 +1,9 @@
 #define __DEPUTY_BOUND(__lo,__hi)              
 #define __DEPUTY_COUNT(__n)                    
 #define __DEPUTY_SINGLE(__n)                   
-#define __DEPUTY_TRUSTED_CAST(__type,__expr)   
+#define __DEPUTY_TRUSTED_CAST(__type,__expr)   ((__type)(__expr))
 
-#define TC(__type,__expr)                      
+#define TC(__type,__expr)                      __DEPUTY_TRUSTED_CAST(__type,__expr)
 
 #define __DEPUTY_NONNULL                       
 #define __DEPUTY_TRUSTED                       
--- tos/lib/safe/include/nesc_annots.h
+++ tos/lib/safe/include/nesc_annots.h
@@ -1,3 +1,4 @@
 struct @bound @deputy_scope() @macro("__DEPUTY_BOUND") { void *lo, *hi; }; 
 struct @count @deputy_scope() @macro("__DEPUTY_COUNT") { int n; }; 
 struct @single @deputy_scope() @macro("__DEPUTY_SINGLE") { }; 
+struct @nonnull @deputy_scope() @macro("__DEPUTY_NONNULL") { }; 
