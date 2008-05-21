e549439f1ac2d3752c448ac7c18486b250585310
scipio <scipio@285f258d-8064-b571-fbc6-9656dc998e90>
2008-05-21 00:50:11 +0000

    NULL
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@3933 285f258d-8064-b571-fbc6-9656dc998e90

--- apps/tests/TestSerialBandwidth/TestSerial.java
+++ apps/tests/TestSerialBandwidth/TestSerial.java
@@ -75,7 +75,7 @@ public class TestSerial implements MessageListener {
   }
   
   public static void main(String[] args) throws Exception {
-    String source = "";
+    String source = null;
     System.out.println("hello");
     if (args.length == 2) {
       if (!args[0].equals("-comm")) {
