6f0789d1416ed9a30ed4a3847a032ae2a49b61e7
idgay <idgay@285f258d-8064-b571-fbc6-9656dc998e90>
2008-06-18 17:16:37 +0000

    move annotations to nesC
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@4082 285f258d-8064-b571-fbc6-9656dc998e90

--- tos/lib/safe/include/annots_stage1.h
+++ tos/lib/safe/include/annots_stage1.h
@@ -1,41 +1,15 @@
+/* Keep TinyOS happy with pre 1.3.0 nesC's */
+
 #ifndef ANNOTS_STAGE1_INCLUDED
 #define ANNOTS_STAGE1_INCLUDED
-#include <stddef.h>
-
-#define __DEPUTY_UNUSED__                      __attribute__((unused))
 
 // define away two obsolete annotations
 #define BOUND(x,y)
 #define SINGLE
 
-#if NESC >= 130
+#if NESC < 130 
 
-struct @nonnull @deputy_scope() @macro("__DEPUTY_NONNULL") { }; 
-struct @bnd @deputy_scope() @macro("__DEPUTY_BND") { void *lo, *hi; }; 
-struct @bnd_nok @deputy_scope() @macro("__DEPUTY_BND_NOK") { void *lo, *hi; }; 
-struct @count @deputy_scope() @macro("__DEPUTY_COUNT") { int n; }; 
-struct @count_nok @deputy_scope() @macro("__DEPUTY_COUNT_NOK") { int n; }; 
-struct @one @deputy_scope() @macro("__DEPUTY_ONE") { }; 
-struct @one_nok @deputy_scope() @macro("__DEPUTY_ONE_NOK") { };
-struct @dmemset @deputy_scope() @macro("__DEPUTY_DMEMSET") {void *p; int what; size_t sz; };
-struct @dmemcpy @deputy_scope() @macro("__DEPUTY_DMEMCPY") {void *dst; void *src; size_t sz; };
-
-#define NONNULL                                @nonnull()
-#define BND(x,y)                               @bnd(x,y)
-#define BND_NOK(x,y)                           @bnd_nok(x,y)
-#define COUNT(x)                               @count(x)
-#define COUNT_NOK(x)                           @count_nok(x)
-#define ONE                                    @one()
-#define ONE_NOK                                @one_nok()
-#define DMEMSET(x,y,z)                         @dmemset(x,y,z)
-#define DMEMCPY(x,y,z)                         @dmemcpy(x,y,z)
-#define TRUSTEDBLOCK                           @unsafe()
-
-#else // NESC < 130
-
-#ifdef SAFE_TINYOS
-#error Safe TinyOS requires nesC >= 1.3.0
-#endif
+#define __DEPUTY_UNUSED__                      __attribute__((unused))
 
 #define NONNULL                                
 #define BND(x,y)                             
@@ -48,19 +22,12 @@ struct @dmemcpy @deputy_scope() @macro("__DEPUTY_DMEMCPY") {void *dst; void *src
 #define DMEMCPY(x,y,z)
 #define TRUSTEDBLOCK
 
-#endif // NESC version check
-
-#ifdef SAFE_TINYOS
-#define TCAST(__type,__expr)                   ((__type)((void * __DEPUTY_TRUSTED __DEPUTY_COPYTYPE)(__expr)))
-#define __DEPUTY_TRUSTED                       __attribute__((trusted))
-#define __DEPUTY_COPYTYPE                      __attribute__((copytype))
-#else
 #define TCAST(__type,__expr)                   ((__type)(__expr))                
-#endif 
 
 #ifdef SAFE_TINYOS
-void * (DMEMSET(1, 2, 3) memset)(void*, int, size_t);
-void * (DMEMCPY(1, 2, 3) memcpy)(void*, const void*, size_t);
+#warning Safe TinyOS requires nesC >= 1.3.0
 #endif
+
+#endif // NESC version check
 
 #endif
