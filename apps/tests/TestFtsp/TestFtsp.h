cd53dbb8f33de2430648ffc75cec055b0b67d1d8
sallai <sallai@285f258d-8064-b571-fbc6-9656dc998e90>
2008-11-03 16:52:43 +0000

    bugfix: skew must be signed
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@4422 285f258d-8064-b571-fbc6-9656dc998e90

--- apps/tests/TestFtsp/TestFtsp.h
+++ apps/tests/TestFtsp/TestFtsp.h
@@ -31,7 +31,7 @@ typedef nx_struct test_ftsp_msg
   nx_uint16_t    counter;
   nx_uint32_t    local_rx_timestamp;
   nx_uint32_t    global_rx_timestamp;
-  nx_uint32_t    skew_times_1000000;
+  nx_int32_t     skew_times_1000000;
   nx_uint8_t     is_synced;
   nx_uint16_t    ftsp_root_addr;
   nx_uint8_t     ftsp_seq;
