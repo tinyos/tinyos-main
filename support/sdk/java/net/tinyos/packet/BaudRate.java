9f244b8852dc273d87a88b59ceb38b0906e2fd49
mmaroti <mmaroti@285f258d-8064-b571-fbc6-9656dc998e90>
2011-10-21 22:53:44 +0000

    ucmini: add default baudrate for ucmini in the java SDK
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@5787 285f258d-8064-b571-fbc6-9656dc998e90

--- support/sdk/java/net/tinyos/packet/BaudRate.java
+++ support/sdk/java/net/tinyos/packet/BaudRate.java
@@ -10,17 +10,18 @@ class BaudRate {
     static void init() throws Exception {
 	/* The Platform.x argument is there for when this code is #include'd
 	   into C */
-	Platform.add(Platform.x, "mica",       19200);
-	Platform.add(Platform.x, "mica2",      57600);
-	Platform.add(Platform.x, "mica2dot",   19200);
+	Platform.add(Platform.x, "mica",        19200);
+	Platform.add(Platform.x, "mica2",       57600);
+	Platform.add(Platform.x, "mica2dot",    19200);
 	Platform.add(Platform.x, "telos",      115200);
 	Platform.add(Platform.x, "telosb",     115200);
-	Platform.add(Platform.x, "tinynode",     115200);
+	Platform.add(Platform.x, "tinynode",   115200);
 	Platform.add(Platform.x, "tmote",      115200);
-	Platform.add(Platform.x, "micaz",      57600);
-	Platform.add(Platform.x, "eyesIFX",       57600);
+	Platform.add(Platform.x, "micaz",       57600);
+	Platform.add(Platform.x, "eyesIFX",     57600);
 	Platform.add(Platform.x, "intelmote2", 115200);
-	Platform.add(Platform.x, "iris",      57600);
-	Platform.add(Platform.x, "shimmer",      115200);
+	Platform.add(Platform.x, "iris",        57600);
+	Platform.add(Platform.x, "shimmer",    115200);
+	Platform.add(Platform.x, "ucmini",     115200);
     }
 }
