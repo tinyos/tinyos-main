86cf23dc7349205f3a0c16741666e9e541ad6875
razvanm <razvanm@285f258d-8064-b571-fbc6-9656dc998e90>
2011-10-24 06:11:46 +0000

    Add the oscilloscope.py to MultihopOscilloscopeLqi
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@5791 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ apps/MultihopOscilloscopeLqi/oscilloscope.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+import tos
+
+AM_OSCILLOSCOPE = 0x93
+
+class OscilloscopeMsg(tos.Packet):
+    def __init__(self, packet = None):
+        tos.Packet.__init__(self,
+                            [('version',  'int', 2),
+                             ('interval', 'int', 2),
+                             ('id',       'int', 2),
+                             ('count',    'int', 2),
+                             ('readings', 'blob', None)],
+                            packet)
+
+if '-h' in sys.argv:
+    print "Usage:", sys.argv[0], "serial@/dev/ttyUSB0:57600"
+    sys.exit()
+
+am = tos.AM()
+
+while True:
+    p = am.read()
+    if p and p.type == AM_OSCILLOSCOPE:
+        msg = OscilloscopeMsg(p.data)
+        print msg.id, msg.count, [i<<8 | j for (i,j) in zip(msg.readings[::2], msg.readings[1::2])]
+        #print msg
+
