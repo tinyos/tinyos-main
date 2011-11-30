c1fe09aa69297d8cf3cd895989010301ab80d88f
mmaroti <mmaroti@285f258d-8064-b571-fbc6-9656dc998e90>
2011-11-30 01:37:07 +0000

    add LedC components to IRIS
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@5829 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ tos/platforms/iris/PlatformLed.h
@@ -0,0 +1,50 @@
+/* Copyright (c) 2010 People Power Co.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ * - Neither the name of the People Power Corporation nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * PEOPLE POWER CO. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE
+ *
+ */
+
+#ifndef _PLATFORMLED_H_
+#define _PLATFORMLED_H_
+
+/** Constants used in the generic LedC module to determine how many
+ * named Led interfaces should be published.  The value should match
+ * the size of the table in PlatformLedsP, but we're not going to
+ * guess that table's public name. */
+#define PLATFORM_LED_COUNT 3
+
+/** Map to the LED index for the red LED */
+#define PLATFORM_LED_RED 0
+
+/** Map to the LED index for the green LED */
+#define PLATFORM_LED_GREEN 1
+
+/** Map to the LED index for the blue LED */
+#define PLATFORM_LED_YELLOW 2
+
+#endif// _PLATFORMLED_H_
