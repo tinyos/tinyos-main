aca4acbb63d41f49bc4c3a003587c666ca746c03
mmaroti <mmaroti@285f258d-8064-b571-fbc6-9656dc998e90>
2012-07-17 19:50:37 +0000

    ucmini: add support for the LedC component
    From: Miklos Maroti <mmaroti@gmail.com>
    
    git-svn-id: http://tinyos-main.googlecode.com/svn/trunk@5976 285f258d-8064-b571-fbc6-9656dc998e90

---
+++ tos/platforms/ucmini/PlatformLed.h
@@ -0,0 +1,43 @@
+/* Copyright (c) 2010 People Power Co.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ * - Neither the name of the People Power Corporation nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * PEOPLE POWER CO. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE
+ *
+ */
+
+#ifndef PLATFORMLED_H
+#define PLATFORMLED_H
+
+#define PLATFORM_LED_COUNT 4
+
+#define PLATFORM_LED_RED 0
+#define PLATFORM_LED_GREEN 1
+#define PLATFORM_LED_YELLOW 2
+#define PLATFORM_LED_ORANGE 3
+
+#endif PLATFORMLED_H
+
