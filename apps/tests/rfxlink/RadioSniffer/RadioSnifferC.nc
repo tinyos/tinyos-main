tinyos/tinyos-main

commit 150356532be3ba10450075f34ce2968214ee5caa

update Tasklet for rf230

tos/chips/rf230/RF230DriverLayerC.nc
@@ -64,13 +64,14 @@ configuration RF230DriverLayerC
 		interface PacketFlag as RSSIFlag;
 		interface PacketFlag as TimeSyncFlag;
 		interface RadioAlarm;
+		interface Tasklet;
 	}
 }
 
 implementation
 {
 	components RF230DriverLayerP as DriverLayerP, 
-		HplRF230C, BusyWaitMicroC, TaskletC, MainC;
+		HplRF230C, BusyWaitMicroC, MainC;
 
 	RadioState = DriverLayerP;
 	RadioSend = DriverLayerP;
@@ -108,7 +109,7 @@ implementation
 	DriverLayerP.RSTN -> HplRF230C.RSTN;
 
 	DriverLayerP.IRQ -> HplRF230C.IRQ;
-	DriverLayerP.Tasklet -> TaskletC;
+	Tasklet = DriverLayerP.Tasklet;
 	DriverLayerP.BusyWait -> BusyWaitMicroC;
 
 #ifdef RADIO_DEBUG

tos/chips/rf230/RF230RadioC.nc
@@ -91,6 +91,10 @@ implementation
 	#define UQ_METADATA_FLAGS	"UQ_RF230_METADATA_FLAGS"
 	#define UQ_RADIO_ALARM		"UQ_RF230_RADIO_ALARM"
 
+// -------- TaskleC
+
+	components new TaskletC();
+
 // -------- RadioP
 
 	components RF230RadioP as RadioP;
@@ -108,6 +112,7 @@ implementation
 
 	components new RadioAlarmC();
 	RadioAlarmC.Alarm -> RadioDriverLayerC;
+	RadioAlarmC.Tasklet -> TaskletC;
 
 // -------- Active Message
 
@@ -221,6 +226,7 @@ implementation
 	MessageBufferLayerC.RadioSend -> CollisionAvoidanceLayerC;
 	MessageBufferLayerC.RadioReceive -> UniqueLayerC;
 	MessageBufferLayerC.RadioState -> TrafficMonitorLayerC;
+	MessageBufferLayerC.Tasklet -> TaskletC;
 	RadioChannel = MessageBufferLayerC;
 
 // -------- UniqueLayer receive part (wired twice)
@@ -310,4 +316,5 @@ implementation
 	RadioDriverLayerC.RSSIFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
 	RadioDriverLayerC.TimeSyncFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
 	RadioDriverLayerC.RadioAlarm -> RadioAlarmC.RadioAlarm[unique(UQ_RADIO_ALARM)];
+	RadioDriverLayerC.Tasklet -> TaskletC;
 }

commit 555c061b9039285ea9db1fced4b44042c4f7476c

fixed compilation error introduced with 150356532be3ba10450075f34ce2968214ee5caa; Updated some rfa1 related stuff

apps/tests/rfxlink/RadioSniffer/RadioSnifferC.nc
@@ -67,10 +67,17 @@ implementation
 	components new MetadataFlagsLayerC();
 	MetadataFlagsLayerC.SubPacket -> RadioDriverLayerC;
 
+// -------- Tasklet
+
+	components new TaskletC();
+
 // -------- RadioAlarm
 
+#if !defined(PLATFORM_UCMINI) && !defined(PLATFORM_UCPROTONB) && !defined(PLATFORM_UCBASE)
 	components new RadioAlarmC();
 	RadioAlarmC.Alarm -> RadioDriverLayerC;
+	RadioAlarmC.Tasklet -> TaskletC;
+#endif
 
 // -------- RadioDriver
 
@@ -83,7 +90,7 @@ implementation
 #elif defined(PLATFORM_MICAZ) || defined(PLATFORM_TELOSA) || defined(PLATFORM_TELOSB)
 	components CC2420XDriverLayerC as RadioDriverLayerC;
 	components CC2420XRadioP as RadioP;
-#elif defined(PLATFORM_UCMINI)
+#elif defined(PLATFORM_UCMINI) || defined(PLATFORM_UCPROTONB) || defined(PLATFORM_UCBASE)
 	components RFA1DriverLayerC as RadioDriverLayerC;
 	components RFA1RadioP as RadioP;
 #endif
@@ -95,5 +102,8 @@ implementation
 	RadioDriverLayerC.TransmitPowerFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
 	RadioDriverLayerC.RSSIFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
 	RadioDriverLayerC.TimeSyncFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
+	RadioDriverLayerC.Tasklet -> TaskletC;
+#if !defined(PLATFORM_UCMINI) && !defined(PLATFORM_UCPROTONB) && !defined(PLATFORM_UCBASE)
 	RadioDriverLayerC.RadioAlarm -> RadioAlarmC.RadioAlarm[unique(UQ_RADIO_ALARM)];
+#endif
 }

apps/tests/rfxlink/TestTransmit/RadioDriverConfigP.nc
@@ -47,10 +47,8 @@ module RadioDriverConfigP
 		interface RF212DriverConfig as RadioDriverConfig;
 #elif defined(PLATFORM_MICAZ) || defined(PLATFORM_TELOSA) || defined(PLATFORM_TELOSB)
 		interface CC2420XDriverConfig as RadioDriverConfig;
-#elif defined(PLATFORM_UCMINI)
+#elif defined(PLATFORM_UCMINI) || defined(PLATFORM_UCPROTONB) || defined(PLATFORM_UCBASE)
 		interface RFA1DriverConfig as RadioDriverConfig;
-#elif defined(PLATFORM_UCDUAL)
-		interface Si443xDriverConfig as RadioDriverConfig;
 #endif
 	}
 }
@@ -72,7 +70,7 @@ implementation
 		return 0;
 	}
 
-#if ! defined(PLATFORM_UCMINI)
+#if !defined(PLATFORM_UCMINI) && !defined(PLATFORM_UCPROTONB) && !defined(PLATFORM_UCBASE)
 	async command uint8_t RadioDriverConfig.headerPreloadLength()
 	{
 		return 7;

apps/tests/rfxlink/TestTransmit/TestRadioDriverC.nc
@@ -67,10 +67,15 @@ implementation
 	components new MetadataFlagsLayerC();
 	MetadataFlagsLayerC.SubPacket -> RadioDriverLayerC;
 
+// -------- Tasklet
+
+	components new TaskletC();
+
 // -------- RadioAlarm
 
 	components new RadioAlarmC();
 	RadioAlarmC.Alarm -> RadioDriverLayerC;
+	RadioAlarmC.Tasklet -> TaskletC;
 
 // -------- RadioDriver
 
@@ -83,12 +88,9 @@ implementation
 #elif defined(PLATFORM_MICAZ) || defined(PLATFORM_TELOSA) || defined(PLATFORM_TELOSB)
 	components CC2420XDriverLayerC as RadioDriverLayerC;
 	components CC2420XRadioP as RadioP;
-#elif defined(PLATFORM_UCMINI)
+#elif defined(PLATFORM_UCMINI) || defined(PLATFORM_UCPROTONB) || defined(PLATFORM_UCBASE)
 	components RFA1DriverLayerC as RadioDriverLayerC;
 	components RFA1RadioP as RadioP;
-#elif defined(PLATFORM_UCDUAL)
-	components Si443xDriverLayerC as RadioDriverLayerC;
-	components Si443xRadioP as RadioP;
 #endif
 
 	components RadioDriverConfigP;
@@ -98,7 +100,8 @@ implementation
 	RadioDriverLayerC.TransmitPowerFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
 	RadioDriverLayerC.RSSIFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
 	RadioDriverLayerC.TimeSyncFlag -> MetadataFlagsLayerC.PacketFlag[unique(UQ_METADATA_FLAGS)];
-#if !defined(PLATFORM_UCMINI)
+	RadioDriverLayerC.Tasklet -> TaskletC;
+#if !defined(PLATFORM_UCMINI) && !defined(PLATFORM_UCPROTONB) && !defined(PLATFORM_UCBASE)
 	RadioDriverLayerC.RadioAlarm -> RadioAlarmC.RadioAlarm[unique(UQ_RADIO_ALARM)];
 #endif
 }