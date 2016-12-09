0376b3190bd2a2d8cf8353e541172cc411b0b8af
Eric B. Decker <cire831@gmail.com>
2016-12-09 04:36:43 -0800

    msp432/i2c: bring in updated I2C.h

--- tos/types/I2C.h
+++ tos/types/I2C.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2005-2006 Arch Rock Corporation
+ * Copyright (c) 2012, Eric B. Decker
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -16,7 +17,7 @@
  *   from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
  * ARCHED ROCK OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
@@ -32,13 +33,37 @@
 /**
  * @author Phil Buonadonna
  * @author Philip Levis
+ * @author Eric B. Decker <cire831@gmail.com>
  */
 
 #ifndef _I2C_H
 #define _I2C_H
 
+/*
+ * It would be nice to deprecate TI2CExtdAddr and TI2CBasicAddr
+ * and replace them with TI2C7Bit and TI2C10Bit.
+ *
+ * But there is way too much code that uses TI2CBasicAddr and BasicAddr
+ * and it would be a pain to fix all that.  Also adding a backward
+ * compatible interface to code providing BasicAddr is a pain because
+ * of the signalling.  More trouble than it is worth.
+ *
+ * So keep in mind that TI2CBasicAddr is really TI2C7Bit.
+ */
+
+/* nobody uses ExtdAddr (10 bit) address but define it anyway */
 typedef struct { } TI2CExtdAddr;
+
+/*
+ * BasicAddr (7 bit addresses) is used all over the place.
+ * so back filling isn't recommended.  New code should use
+ * TI2C7Bit.  But that is also problematic.  If so stick with
+ * TI2CBasicAddr.
+ */
 typedef struct { } TI2CBasicAddr;
+
+typedef struct { } TI2C7Bit;
+typedef struct { } TI2C10Bit;
 
 typedef uint8_t i2c_flags_t;
 
@@ -46,6 +71,7 @@ enum {
   I2C_START   = 0x01,
   I2C_STOP    = 0x02,
   I2C_ACK_END = 0x04,
+  I2C_RESTART = 0x08,
 };
 
 
